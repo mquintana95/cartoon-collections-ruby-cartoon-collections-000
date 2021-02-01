88f1abe112bf2d9de67306da5cb290f3270e9a07
Marcus Quintana <mq97325@gmail.com>
2021-02-01 17:03:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |item,index|
-  puts "#{index+1}:#{item}"
-  end
-end
+# Method 1 - print dwarves names in number order
+ def roll_call_dwarves(dwarves)
+   new_array = []
+   dwarves.collect do |item,index|
+   puts "#{index+1}:#{item}" 
+   end
+ end
 
+# Method 2 - capitalize each element + add exclamation point to end of each
 def summon_captain_planet(array)
   new_array = []
   i = 0 
@@ -14,6 +17,7 @@ def summon_captain_planet(array)
   new_array
 end
 
+# Method 3 - check for calls longer than 4 characters
 def long_planeteer_calls(array) 
   i = 0 
   if  array.any? {|i| i.length > 4}
@@ -24,9 +28,23 @@ def long_planeteer_calls(array)
   end 
 end
 
+# Method 4 - find and return type of cheese as first string
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
     cheese_types.include?(type)
   end 
 end
+
+def word_with_b(array)
+  new_array = []
+  array.select do |b|
+  if b.start_with?("b")
+    new_array.push(b)
+  end
+  end
+  new_array
+end
+
+
+
